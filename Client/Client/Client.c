a86f7284039e1155dcfeb7b55de3422c6a995f86
Lena Goldmann <lenagoldmann@yahoo.de>
2021-03-12 00:01:38 +0100

    cleanup and documentation

--- Client/Client/Client.c
+++ Client/Client/Client.c
@@ -6,97 +6,68 @@
 #include <string.h>
 #include <Winsock2.h>
 #include <ws2tcpip.h>
+#include "Packet.h"
 
 #define DEFAULT_FAMILY      AF_INET6    //Protocol family - in this case force IPv6
 #define DEFAULT_SOCKTYPE    SOCK_STREAM //TCP uses SOCK_STREAM, UDP uses SOCK_DGRAM
-#define BUFFER_SIZE         1024        //The buffer size for the demonstration
 
-//data structure that will be sent to the server
-struct package {
-    char sNumber[7];
-    char txt[BUFFER_SIZE];
-}; 
+//initialise TCP for Windows ("Winsock")
+void initialiseWinsock() {
+    int returnValue;
 
-char* init_package(struct package p) {
-    char* package_ZK = NULL;
-    int sNumberLength = strlen(p.sNumber);
-    int txtLength = strlen(p.txt);
-    int totalLength = sNumberLength + 2 + txtLength - 1;
-    char spacer[2] = { ':', ' ' };
-
-    package_ZK = (char*)malloc(totalLength * sizeof(char)); //allocate memory for the package
-
-    if (package_ZK == NULL) {
-        printf("Error on building package.\n");
-        EXIT_FAILURE;
-    }
-
-    int i = 0;
-    int j = 0;
-
-    for (i; i < sNumberLength; i++) {
-        package_ZK[i] = p.sNumber[i];
-    }
-
-    package_ZK[i] = spacer[0];
-    i++;
-    package_ZK[i] = spacer[1];
-    i++;
-    
-    for (j = 0, i; j <= txtLength; i++, j++) {
-        package_ZK[i] = p.txt[j];
-    }
-
-    return package_ZK;
-}
-
-int main(int argc, char* argv[]) {
-    char receivebuffer[BUFFER_SIZE];
-    char sendbuffer[BUFFER_SIZE];
-    SOCKET ClientSocket = INVALID_SOCKET;
-    struct addrinfo* result = NULL, hints;
-    int iResult;
-    int bufferLength = BUFFER_SIZE;
-
-    if (argc < 3) {
-        fprintf(stderr, "Application needs IP adress, port and S-Number as arguments.\n");
-        exit(1);
-    }
-
-    const char* ip = argv[1];
-    const char* port = argv[2];
-    const char* sNumber = argv[3];
-
-    //initialise TCP for Windows ("Winsock")
     WORD wVersionRequested; //WORD = unsigned long data type, version of the Winsocket
     WSADATA wsaData; //stores information about the Winsocket implementation
     wVersionRequested = MAKEWORD(2, 2); //MAKEWORD = macro to request version 2.2
 
-    iResult = WSAStartup(wVersionRequested, &wsaData); //makes it possible for the process to run WINSOCK.DLL
-    if (iResult != 0) {
+    returnValue = WSAStartup(wVersionRequested, &wsaData); //makes it possible for the process to run WINSOCK.DLL
+    if (returnValue != 0) {
         printf("Error on initialising Winsock.\n");
         exit(1);
     }
     else {
         printf("Winsock initialised.\n");
     }
+}
+
+int main(int argc, char* argv[]) {
+    SOCKET clientSocket = INVALID_SOCKET;
+    struct addrinfo* result = NULL, hints;
+    int returnValue;
+    struct Packet message_cli;
+    struct Packet message_serv;
+   
+    if (argc < 4) {
+        fprintf(stderr, "Application needs IP adress, port and S-Number as arguments.\nUsage: Client.exe ::1 1234 s12345\n");
+        exit(1);
+    }
+    if (argc > 4) {
+        fprintf(stderr, "You have entered too many arguments. This application only accepts 3 arguments. Anything else will be ignored.\n");
+    }
+
+    //initalise datastruct
+    strcpy(message_cli.text, " ");
+    strcpy(message_cli.sNumber, argv[3]);
+    
+    const char* ip = argv[1];
+    const char* port = argv[2];
+
+    initialiseWinsock();
 
     ZeroMemory(&hints, sizeof(hints)); //Macro to fill a block of memory with zeros
     hints.ai_family = DEFAULT_FAMILY;
     hints.ai_socktype = DEFAULT_SOCKTYPE;
     hints.ai_protocol = IPPROTO_TCP; //specifies the TCP protocol
 
-    iResult = getaddrinfo(ip, port, &hints, &result);
-    if (iResult != 0) {
-        printf("getaddrinfo failed: %d\n", iResult);
+    returnValue = getaddrinfo(ip, port, &hints, &result);
+    if (returnValue != 0) {
+        printf("getaddrinfo failed: %d\n", returnValue);
         WSACleanup();
         exit(1);
     }
 
     //create a socket that will connect to the server
-    ClientSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-
-    if (ClientSocket == INVALID_SOCKET) {
+    clientSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (clientSocket == INVALID_SOCKET) {
         printf("Error at socket(): %ld\n", WSAGetLastError());
         freeaddrinfo(result);
         WSACleanup();
@@ -104,16 +75,16 @@ int main(int argc, char* argv[]) {
     }
 
     //connect to the server
-    iResult = connect(ClientSocket, result->ai_addr, (int)result->ai_addrlen);
-    if (iResult == SOCKET_ERROR) {
-        closesocket(ClientSocket);
-        ClientSocket = INVALID_SOCKET;
+    returnValue = connect(clientSocket, result->ai_addr, (int)result->ai_addrlen);
+    if (returnValue == SOCKET_ERROR) {
+        closesocket(clientSocket);
+        clientSocket = INVALID_SOCKET;
     }
 
     //free the address information that getaddrinfo has returned
     freeaddrinfo(result);
 
-    if (ClientSocket == INVALID_SOCKET) {
+    if (clientSocket == INVALID_SOCKET) {
         printf("Unable to connect to server.\n");
         WSACleanup();
         exit(1);
@@ -122,30 +93,23 @@ int main(int argc, char* argv[]) {
     printf("You are now connected.\n");
 
     while (1) {
-        gets(sendbuffer);
-
-        struct package p; //build new data package
-        strcpy(p.sNumber, sNumber); //fill data package with data
-        strcpy(p.txt, sendbuffer);
-
-        char* package_ZK = NULL;
-        package_ZK = init_package(p); //pointer to the built data package
-
-        iResult = send(ClientSocket, package_ZK, (int)strlen(package_ZK), 0);
-        if (iResult == 0 || iResult == SOCKET_ERROR) {
+        printf("%s> ", &message_cli.sNumber);
+        gets(&message_cli.text);
+        returnValue = send(clientSocket, &message_cli, sizeof(message_cli), 0);
+        if (returnValue == 0 || returnValue == SOCKET_ERROR) {
             printf("Server has disconnected.\n");
-            closesocket(ClientSocket);
+            closesocket(clientSocket);
             WSACleanup();
             exit(1);
         }
 
-        iResult = recv(ClientSocket, receivebuffer, bufferLength, 0);
-        if (iResult > 0) {
-            receivebuffer[iResult] = '\0';
-            printf("%s\n", receivebuffer);
+        returnValue = recv(clientSocket, &message_serv, sizeof(message_serv), 0);
+        if (returnValue > 0) {
+            printf("%s> %s\n",message_serv.sNumber, message_serv.text);
         }
     }
-    closesocket(ClientSocket);
+
+    closesocket(clientSocket);
     WSACleanup();
     return 0;
 }
